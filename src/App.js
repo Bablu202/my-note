50e907f8ac37042d4fbfd62dddfd64f2c79cae08
aware <aware9758@gmail.com>
2024-01-24 16:16:38 +0100

    handle

--- src/App.js
+++ src/App.js
@@ -1,67 +1,89 @@
+import { useState } from "react";
 import "./sass/main.scss";
 export default function App() {
+  // const [monthlyExpenses, setMonthlyExpenses] = useState([
+  //   { id: 1, expenseName: "rent", priceValue: 550 },
+  //   { id: 2, expenseName: "Travel", priceValue: 120.5 },
+  // ]);
+  const [expenses, setExpenses] = useState([
+    { id: 1, expenseName: "food", priceValue: "35.89" },
+    { id: 2, expenseName: "games", priceValue: "22.5" },
+  ]);
+
   function handleAddExpense(e) {
     e.preventDefault();
-    console.log("add");
+    console.log("added");
+    let idWithTime = Date.now();
+    let newExpense = {
+      id: idWithTime,
+      expenseName: expenses.expenseName,
+      priceValue: expenses.priceValue,
+    };
+    setExpenses((prev) => [...prev, newExpense]);
   }
+
   return (
     <div className="app">
-      {/* Main */}
+      {/* Header */}
       <div className="main">
         <div className="header">
           <h3 className="header-name">flow</h3>
           <p className="header-amount">0.00</p>
         </div>
-        <div className="sub">
-          <ul className="sub-lists">
-            <li className="sub-list">
-              <span className="sub-list--name">one</span>
-              <span className="sub-list--price">0.00</span>
-            </li>
-            <li className="sub-list">
-              <span className="sub-list--name">two</span>
-              <span className="sub-list--price">0.00</span>
-            </li>
-          </ul>
-        </div>
+        {/* monthly things */}
+        <>
+          <div className="sub">
+            <ul className="sub-lists">
+              {/* {monthlyExpenses.map((e) => (
+                <li className="sub-list" key={e.id}>
+                  <span className="sub-list--name">{e.expenseName}</span>
+                  <span className="sub-list--price">${e.priceValue}</span>
+                </li>
+              ))} */}
+            </ul>
+          </div>
+        </>
       </div>
       {/* list */}
-      <div className="bills-box">
-        <ul className="bills-box--list">
-          {/* sub-list */}
-          <li className="bill">
-            <span className="bill-name">name of the expense </span>
-            <span className="bill-price">-1.00$</span>
-          </li>
-          <li className="bill">
-            <span className="bill-name">name of the expense </span>
-            <span className="bill-price">-2.50$</span>
-          </li>
-          <li className="bill">
-            <span className="bill-name">name of the expense </span>
-            <span className="bill-price">-35.85$</span>
-          </li>
-        </ul>
-      </div>
+      {/* sub-list */}
+      <>
+        <div className="bills-box">
+          <ul className="bills-box--list">
+            {expenses.map((e) => (
+              <li className="bill" key={e.id}>
+                <span className="bill-name">{e.expenseName}</span>
+                <span className="bill-price">-{e.priceValue}$</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+
       {/* form */}
-      <form className="form" onSubmit={handleAddExpense}>
+      <form className="form">
         <div className="form-input">
           <input
             placeholder="spent on.."
             className="form-input--name"
             type="text"
-            id="expense"
-            required
+            value={expenses.expenseName}
+            onChange={(e) =>
+              setExpenses({ ...expenses, expenseName: e.target.value })
+            }
           />
           <input
             type="number"
-            step="0.01"
-            placeholder="0.00"
+            // step="0.01"
+            // placeholder="0.00"
             className="form-input--price"
-            id="price"
-            required
+            value={expenses.priceValue}
+            onChange={(e) =>
+              setExpenses({ ...expenses, priceValue: e.target.value })
+            }
           />
-          <button className="form-input--btn btn">$ add</button>
+          <button className="form-input--btn btn" onClick={handleAddExpense}>
+            $ add
+          </button>
         </div>
       </form>
     </div>
